856cffd847e0e048a79580dcb83416fc617f178e
Sheikhman_Kalderon <duah229@outlook.com>
2025-11-10 11:41:37 +0000

    feature: add demonstrable usage of the final keyword

---
+++ variables/final_keyword.dart
@@ -0,0 +1,11 @@
+void main() {
+  //usages of final, it is also termed as restrictive var and you can omit type
+
+  //perfect
+  final trial = 34;
+  print(trial);
+
+  //unnecessary to add type
+  final String test = "Kwaku";
+  print(test);
+}
---
+++ variables/late_keyword.dart
@@ -0,0 +1,11 @@
+// if you dont want to initialize a variable immediately use the late keyword
+
+// void main() {
+//   late List<String> names;
+//
+//   if(iWantFriends()) {
+//     names = friends.getNames();
+//     else
+//       names = haters.getNames();
+//   }
+// }
