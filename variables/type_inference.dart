5dd73ca1a70934ccc13aa18b6ef90a20762b3688
Sheikhman_Kalderon <duah229@outlook.com>
2025-11-10 10:27:42 +0000

    feature: add types of variable instantiation types in dart

---
+++ variables/type_inference.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  //type inference
+  var value = 18;
+  var myName = "Alberto";
+
+  // strong type inference
+  int val = 20;
+  String myGroup = "Television";
+
+
+ print(value); print(val); print(myGroup);
+ print(myName);
+}
+
+
+
