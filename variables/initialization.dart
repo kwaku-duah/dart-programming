b19d1aedfdec7e738407ac5e06269facc9ccd183
Sheikhman_Kalderon <duah229@outlook.com>
2025-11-10 11:05:08 +0000

    feature: add demonstration of why var is the best to use to declare variables

---
+++ variables/initialization.dart
@@ -0,0 +1,19 @@
+void main() {
+
+  //this is not easy to read
+
+  // List<List<Toppings>> pizza = List<List<Toppings>>();
+  // for (List<Toppings> topping in pizza) {
+  //   doSomething(topping);
+  // }
+
+
+  //easier on the eyes
+  // var pizza = List<List<Toppings>>();
+  // for (var topping in pizza) {
+  //   doSomething(topping);
+  // }
+}
+
+class Toppings {
+}
