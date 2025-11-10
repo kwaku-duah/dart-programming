f85ee283ee0775d30e4e1a4f156cbce63356ec1a
Sheikhman_Kalderon <duah229@outlook.com>
2025-11-10 10:38:43 +0000

    feature: add another way of defining variables

--- variables/type_inference.dart
+++ variables/type_inference.dart
@@ -8,6 +8,9 @@ void main() {
   int val = 20;
   String myGroup = "Television";
 
+  // another type of variable declaration (tells the compiler to find out)
+  dynamic values = 20;
+  dynamic mySystem = 25;
 
  print(value); print(val); print(myGroup);
  print(myName);
