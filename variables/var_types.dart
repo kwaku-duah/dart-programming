29b9d7996ecf0b0eade37ce522d04a856bd09c37
Sheikhman_Kalderon <duah229@outlook.com>
2025-11-10 12:07:23 +0000

    feature: ad usage of dynamic/f-string equivalence in dart

---
+++ variables/var_types.dart
@@ -0,0 +1,10 @@
+void main() {
+  //variable types in dart
+
+  var age = 25;
+  var myAge = "I am $age years of age";
+  print(myAge);
+
+  // this is for expressions, wrap it {}
+  var test = "${25.abs()}";
+}
